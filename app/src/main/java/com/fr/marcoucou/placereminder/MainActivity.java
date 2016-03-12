4cbf2914f39ccf2602d922bc9fb836e960ba2cba
marcwadai <marc.wadai@gmail.com>
2016-03-12 16:09:00 +0100

    beginning of navigation drawer

---
+++ app/src/main/java/com/fr/marcoucou/placereminder/MainActivity.java
@@ -0,0 +1,161 @@
+import com.fr.marcoucou.placereminder.*;
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+
+import com.fr.marcoucou.placereminder.R;
+import com.fr.marcoucou.placereminder.adapter.NavigationDrawerAdapter;
+import com.fr.marcoucou.placereminder.model.NavigationDrawer;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    // nav drawer title
+    private CharSequence mDrawerTitle;
+
+    // used to store app title
+    private CharSequence mTitle;
+
+    // slide menu items
+    private String[] navMenuTitles;
+    private TypedArray navMenuIcons;
+
+    private ArrayList<NavigationDrawer> navDrawerItems;
+    private NavigationDrawerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTitle = mDrawerTitle = getTitle();
+
+        // load slide menu items
+        navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
+        // nav drawer icons from resources
+        //navMenuIcons = getResources()
+        //        .obtainTypedArray(R.array.nav_drawer_icons);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
+
+        navDrawerItems = new ArrayList<NavigationDrawer>();
+
+        // adding nav drawer items to array
+        // Home
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[0], R.drawable.ic_home));
+        // Find People
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[1], R.drawable.ic_home));
+        // Photos
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[2], R.drawable.ic_home));
+        // Communities, Will add a counter here
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[3], R.drawable.ic_home));
+        // Pages
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[4],R.drawable.ic_home));
+        // What's hot, We  will add a counter here
+        navDrawerItems.add(new NavigationDrawer(navMenuTitles[5], R.drawable.ic_home));
+
+
+        // Recycle the typed array
+        navMenuIcons.recycle();
+
+        // setting the nav drawer list adapter
+        adapter = new NavigationDrawerAdapter(getApplicationContext(),
+                navDrawerItems);
+        mDrawerList.setAdapter(adapter);
+
+        // enabling action bar app icon and behaving it as toggle button
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.drawable.ic_home, //nav menu toggle icon
+                R.string.app_name, // nav drawer open - description for accessibility
+                R.string.app_name // nav drawer close - description for accessibility
+        ) {
+            public void onDrawerClosed(View view) {
+                getActionBar().setTitle(mTitle);
+                // calling onPrepareOptionsMenu() to show action bar icons
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getActionBar().setTitle(mDrawerTitle);
+                // calling onPrepareOptionsMenu() to hide action bar icons
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState == null) {
+            // on first time display view for first nav item
+            //displayView(0);
+        }
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // toggle nav drawer on selecting action bar app icon/title
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action bar actions click
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /***
+     * Called when invalidateOptionsMenu() is triggered
+     */
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // if nav drawer is opened, hide the action items
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
---
+++ app/src/main/java/com/fr/marcoucou/placereminder/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,72 @@
+package com.fr.marcoucou.placereminder.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.fr.marcoucou.placereminder.R;
+import com.fr.marcoucou.placereminder.model.NavigationDrawer;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Marc on 11/03/2016.
+ */
+public class NavigationDrawerAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<NavigationDrawer> navDrawerItems;
+
+    public NavigationDrawerAdapter(Context context, ArrayList<NavigationDrawer> navDrawerItems){
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater)
+                    context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.navdrawer_list_item, null);
+        }
+
+        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+        TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
+        TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
+
+        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+        txtTitle.setText(navDrawerItems.get(position).getTitle());
+
+        // displaying count
+        // check whether it set visible or not
+        if(navDrawerItems.get(position).getCounterVisibility()){
+            txtCount.setText(navDrawerItems.get(position).getCount());
+        }else{
+            // hide the counter view
+            txtCount.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+}
